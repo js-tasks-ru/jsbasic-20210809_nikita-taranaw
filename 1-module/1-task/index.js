bd3fd8e108c1cbdb3c71cdc0780eb30c36ed5366
ntaranaw <nikita.taranaw@gmail.com>
2021-08-16 17:53:51 +0300

    1 module 1 task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+  while (n > 0) {
+    result = result * n;
+    n = n - 1;
+  }
+  return result;
 }
+
+factorial(0);
+factorial(1);
+factorial(3);
+factorial(5);
