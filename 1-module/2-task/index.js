0b98723ab84476247915665ff8c67a18f0861cab
ntaranaw <nikita.taranaw@gmail.com>
2021-08-18 13:38:12 +0300

    1 module 2 task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -9,16 +9,15 @@ function print(text) {
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
-function isValid(name) {
-  // ваш код...
+ function isValid(name) {
+  return !!name && !name.includes(' ') && name.length >= 4;
 }
-
 function sayHello() {
-  let userName = prompt('Введите ваше имя');
+  let userName = prompt("Введите ваше имя");
 
   if (isValid(userName)) {
     print(`Welcome back, ${userName}!`);
   } else {
-    print('Некорректное имя');
+    print("Некорректное имя");
   }
 }
