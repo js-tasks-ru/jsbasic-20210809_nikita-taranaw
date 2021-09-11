74a12d3ebaae22bc98745e08911010f4e2ca3fa8
ntaranaw <nikita.taranaw@gmail.com>
2021-09-11 22:52:19 +0300

    2 module

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,10 @@
 function isEmpty(obj) {
-  // ваш код...
+  return !(Object.keys(obj).length > 0);
 }
+
+// function isEmpty(obj) {
+//   for (key in obj) {
+//     return false;
+//   }
+//   return true;
+// }
